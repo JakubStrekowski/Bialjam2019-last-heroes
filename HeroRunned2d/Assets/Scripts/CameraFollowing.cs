ae86caefe05f34f154544b76598c17ad55861f71
Adam Szymanski <asz.szymanski@gmail.com>
2019-06-07 20:45:29 +0200

    Fix camera follow

--- HeroRunned2d/Assets/Scripts/CameraFollowing.cs
+++ HeroRunned2d/Assets/Scripts/CameraFollowing.cs
@@ -3,22 +3,13 @@ using System.Collections;
 
 public class CameraFollowing : MonoBehaviour
 {
+    public float FollowSpeed = 4f;
+    public Transform Target;
 
-    public float dampTime = 0.15f;
-    private Vector3 velocity = Vector3.zero;
-    public Transform target;
-    public Camera camera;
-
-    // Update is called once per frame
     void Update()
     {
-        if (target)
-        {
-            Vector3 point = camera.WorldToViewportPoint(target.position);
-            Vector3 delta = target.position - camera.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z)); //(new Vector3(0.5, 0.5, point.z));
-            Vector3 destination = transform.position + delta;
-            transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
-        }
-
+        Vector3 newPosition = Target.position;
+        newPosition.z = -10;
+        transform.position = Vector3.Slerp(transform.position, newPosition, FollowSpeed * Time.deltaTime);
     }
 }
