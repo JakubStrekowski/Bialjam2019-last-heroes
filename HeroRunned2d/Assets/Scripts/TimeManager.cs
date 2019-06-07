bc0bc9ae3fe46165b0186619880ea7c0b2a5032d
Jacopo <Jakub1403@gmail.com>
2019-06-07 18:09:03 +0200

    Added lose condition

--- HeroRunned2d/Assets/Scripts/TimeManager.cs
+++ HeroRunned2d/Assets/Scripts/TimeManager.cs
@@ -1,18 +1,67 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TimeManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float TimeToLose
     {
-        
+        get { return time; }
+        set
+        {
+            time = value;
+            if(time>0)
+            timeTxt.text = time.ToString("f1");
+            else
+            {
+                timeTxt.text = "XX,X";
+                timeTxt.color = Color.red;
+            }
+        }
+    }
+    public TextMeshProUGUI timeTxt;
+    public GameObject gameLostPanel;
+
+    float time;
+    bool gameLost = false;
+    private void Start()
+    {
+        time = 5;
+        StartCoroutine("CountDown");
+    }
+    private void Awake()
+    {
+        gameLostPanel.SetActive(false);
+    }
+    private void Update()
+    {
+        if (Input.GetAxis("Submit") > 0&&gameLost)
+        {
+            Debug.Log("lol");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void LoseGame()
     {
-        
+        Debug.Log("Game Lost");
+        gameLostPanel.SetActive(true);
     }
+
+    IEnumerator CountDown()
+    {
+        while (TimeToLose > 0)
+        {
+            TimeToLose -= 0.1f;
+            yield return new WaitForSeconds(0.1f);
+        }
+        if (gameLost == false)
+        {
+            gameLost = true;
+            LoseGame();
+        }
+    }
+
 }
