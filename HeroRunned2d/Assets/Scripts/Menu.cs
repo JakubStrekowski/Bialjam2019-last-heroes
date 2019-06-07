d8ac47f8544678d1504ea50a7775292358a4e076
Archer141 <ezio.141.96@gmail.com>
2019-06-07 21:03:23 +0200

    Menu alpha, opions menu works

---
+++ HeroRunned2d/Assets/Scripts/Menu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+
+public class Menu : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+
+   public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    } 
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void ChangeVolume(float volume)
+    {
+        audioMixer.SetFloat("volumeXD", volume);
+    }
+}
