c8fcd12b335103f2609bcde4a6e82c84dc58c1e9
Jacopo <Jakub1403@gmail.com>
2019-06-07 20:24:32 +0200

    Added tank moving boxes functionality

--- HeroRunned2d/Assets/Scripts/Box.cs
+++ HeroRunned2d/Assets/Scripts/Box.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class Box : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Rigidbody2D rb;
+
+    private void Start()
     {
-        
+        rb.gravityScale = 10f;
+        rb.mass = 200;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        if (collision.collider.tag == "Hero")
+        {
+            if (collision.gameObject.GetComponent<Tank>() != null)
+            {
+                rb.gravityScale = 1;
+                rb.mass = 2;
+            }
+        }
     }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Hero")
+        {
+            if (collision.gameObject.GetComponent<Tank>() != null)
+            {
+                rb.gravityScale = 10f;
+                rb.mass = 200;
+            }
+        }
+    }
+
 }
--- HeroRunned2d/Assets/Scripts/Heroes/Tank.cs
+++ HeroRunned2d/Assets/Scripts/Heroes/Tank.cs
@@ -2,17 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Tank : MonoBehaviour
+public class Tank : Hero
 {
+    private void Awake()
+    {
+        base.Awake();
+    }
     // Start is called before the first frame update
-    void Start()
+    
+    private void FixedUpdate()
     {
-        
+        base.FixedUpdate();
     }
-
     // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
+
 }
