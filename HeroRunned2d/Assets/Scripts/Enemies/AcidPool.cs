256a446b3fcef83a3ac0aa5836de837763766b05
Adam Szymanski <asz.szymanski@gmail.com>
2019-06-08 23:56:30 +0200

    Add acid pool

---
+++ HeroRunned2d/Assets/Scripts/Enemies/AcidPool.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AcidPool : MonoBehaviour
+{
+    private SpriteRenderer spriteRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(nameof(Flash));
+    }
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Hero"))
+        {
+            var hero = other.gameObject.GetComponent<Hero>();
+            hero.Die();
+        }
+    }
+    
+    private IEnumerator Flash()
+    {
+        var color = spriteRenderer.color;
+        while (true)
+        {
+            for (float i = 0.6f; i <= 1f; i += 0.01f)
+            {
+                spriteRenderer.color = new Color(color.r, color.g, color.b, i);
+                yield return new WaitForSeconds(0.02f);
+            }
+
+            for (float i = 1f; i >= 0.6f; i -= 0.01f)
+            {
+                spriteRenderer.color = new Color(color.r, color.g, color.b, i);
+                yield return new WaitForSeconds(0.02f);
+            }
+        }
+    }
+}
