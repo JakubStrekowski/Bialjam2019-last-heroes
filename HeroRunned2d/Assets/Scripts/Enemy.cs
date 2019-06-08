d98447d8cc760d59a039ba21f0058c3e27306eaf
Jacopo <Jakub1403@gmail.com>
2019-06-08 11:51:01 +0200

    Added enemy prefab

--- HeroRunned2d/Assets/Scripts/Enemy.cs
+++ HeroRunned2d/Assets/Scripts/Enemy.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    public Transform[] waypoints;
+    public float movementSpeed = 3f;
+    public Rigidbody2D rb;
+    int currentWaypoint;
+    float moveDirection;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentWaypoint = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
+        float distance =Mathf.Abs( waypoints[currentWaypoint].position.x - transform.position.x);
         
+        if (distance < 0.2f)
+        {
+            currentWaypoint = (currentWaypoint + 1) % waypoints.Length;
+        }
+
+        float direction = (waypoints[currentWaypoint].position.x - transform.position.x);
+        if (direction < 0)
+        {
+            moveDirection = -1;
+        }
+        else
+        {
+            moveDirection = 1;
+        }
+        
+        rb.velocity = new Vector2(moveDirection * movementSpeed, rb.velocity.y);
     }
+
 }
