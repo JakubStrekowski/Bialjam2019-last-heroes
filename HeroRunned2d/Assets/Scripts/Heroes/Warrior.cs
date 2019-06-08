22c80a5482b20bbf5c60ee713cb17428e937a294
Adam Szymanski <asz.szymanski@gmail.com>
2019-06-08 11:27:52 +0200

    Rename hero prefabs, add instant hero swap

--- HeroRunned2d/Assets/Scripts/Heroes/Warrior.cs
+++ HeroRunned2d/Assets/Scripts/Heroes/Warrior.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Warrior : MonoBehaviour
+public class Warrior : Hero
 {
     // Start is called before the first frame update
     void Start()
