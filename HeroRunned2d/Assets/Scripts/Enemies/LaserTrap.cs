524755539645dc79c77f2d5b928aee00eab3c55c
Adam Szymanski <asz.szymanski@gmail.com>
2019-06-08 23:49:46 +0200

    Fix laser trap freeze

--- HeroRunned2d/Assets/Scripts/Enemies/LaserTrap.cs
+++ HeroRunned2d/Assets/Scripts/Enemies/LaserTrap.cs
@@ -4,15 +4,18 @@ using System.Collections.Generic;
 using UnityEditor.U2D;
 using UnityEngine;
 
-public class LaserTrap : MonoBehaviour
+public class LaserTrap : BaseEnemy
 {
     private SpriteRenderer spriteRenderer;
     private Collider2D collider2d;
+    private bool coroutinesStared = false;
 
     // Start is called before the first frame update
     void Start()
     {
+        StartCoroutine(nameof(Switch));
         StartCoroutine(nameof(Flash));
+        coroutinesStared = true;
     }
 
     private void Awake()
@@ -27,6 +30,26 @@ public class LaserTrap : MonoBehaviour
         
     }
 
+    public override void Stop()
+    {
+        if (coroutinesStared)
+        {
+            StopCoroutine(nameof(Switch));
+            StopCoroutine(nameof(Flash));
+            coroutinesStared = false;
+        }
+    }
+
+    public override void Resume()
+    {
+        if (!coroutinesStared)
+        {
+            StartCoroutine(nameof(Switch));
+            StartCoroutine(nameof(Flash));
+            coroutinesStared = true;
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Hero"))
@@ -36,25 +59,43 @@ public class LaserTrap : MonoBehaviour
         }
     }
 
-    private IEnumerator Flash()
+    private IEnumerator Switch()
     { 
-        var color = spriteRenderer.color;
         while (true)
         {
             collider2d.enabled = true;
-            spriteRenderer.color = new Color(color.r, color.g, color.b, 180f);
+            spriteRenderer.enabled = true;
             yield return new WaitForSeconds(3f);
             collider2d.enabled = false;
-            spriteRenderer.color = new Color(color.r, color.g, color.b, 0f);
+            spriteRenderer.enabled = false;
             yield return new WaitForSeconds(2f);
-            spriteRenderer.color = new Color(color.r, color.g, color.b, 180f);
+            spriteRenderer.enabled = true;
             yield return new WaitForSeconds(0.1f);
-            spriteRenderer.color = new Color(color.r, color.g, color.b, 0f);
+            spriteRenderer.enabled = false;
             yield return new WaitForSeconds(0.1f);
-            spriteRenderer.color = new Color(color.r, color.g, color.b, 180f);
+            spriteRenderer.enabled = true;
             yield return new WaitForSeconds(0.1f);
-            spriteRenderer.color = new Color(color.r, color.g, color.b, 0f);
+            spriteRenderer.enabled = false;
             yield return new WaitForSeconds(0.1f);
+        }
+    }
+
+    private IEnumerator Flash()
+    {
+        var color = spriteRenderer.color;
+        while (true)
+        {
+            for (float i = 0.3f; i <= 1f; i += 0.02f)
+            {
+                spriteRenderer.color = new Color(color.r, color.g, color.b, i);
+                yield return new WaitForSeconds(0.01f);
+            }
+
+            for (float i = 1f; i >= 0.3f; i -= 0.02f)
+            {
+                spriteRenderer.color = new Color(color.r, color.g, color.b, i);
+                yield return new WaitForSeconds(0.01f);
+            }
         }
     }
 }
